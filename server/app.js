fd8a7b7f89e98b51352d78fb229e67d578aaa91a
Quynh Chi Lai <quynhchi1009@yandex.com>
2023-04-24 18:10:57 +0200

    Update app.js

--- server/app.js
+++ server/app.js
@@ -13,7 +13,7 @@ const mongoDataMethods = require('./data/db')
 //Connect to MongoDB
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb+srv://quynhchilai:0811Blue@tutorial01.adjuhty.mongodb.net/?retryWrites=true&w=majority',
+        await mongoose.connect('mongodb+srv://quynhchilai:********@tutorial01.adjuhty.mongodb.net/?retryWrites=true&w=majority',
         {})
         console.log('MongoDB connected');
     } catch (error) {
